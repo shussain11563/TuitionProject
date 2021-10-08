b4c3055240ec3d8b0bf6d4f1c34cf49efb546dca
shussain11563 <44513446+shussain11563@users.noreply.github.com>
2021-10-08 15:43:32 -0400

    Comment

--- src/Student.java
+++ src/Student.java
@@ -1,14 +1,17 @@
 public class Student {
-    private int tuitionDue;
+    //private int tuitionDue;
+    //TuitionPay
     private Profile profile;
 
     /**
+     * CHANGE THIS JAVA DOCS!
      * Constructs and initializes an Student object for temporary use.
      * Used for lending, removing, and returning an Album.
      * @param title the title of the Album.
      * @param artist the artist of the Album.
      */
-    public Student() {
+    public Student()
+    {
 
     }
 

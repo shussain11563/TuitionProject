cc9e65cba9d8b2f68a6792f27ba391b74311ed92
David Lam <dlamh19@gmail.com>
2021-10-10 14:07:51 -0400

    Did small changes

--- src/Roster.java
+++ src/Roster.java
@@ -196,37 +196,8 @@ public class Roster {
         }
     }
 
-
-
-    private void calculateTuition()
+    public void calculateTuition()
     {
-        /*
-            check if its resident, non resident, or international for base tuition payment
-            add university fee to all students
-            resident and non resident has no additional fees
-            international has additional fees
-
-            if credit > 16 then
-                resident adds $404 per credit
-                non resident adds $966 per credit
-
-            if part time student (< 12 credits)
-                resident adds $404 per credit
-                non resident adds $966 per credit
-                univesity fee is 80% of $3268
-
-             ___________
-             Remissions
-             NY - $4000 discount
-             CT - $5000 discount
-
-             If International = Student Abroad then no tuition
-                needs to pay the $3268 fee + international fee of 2650
-
-
-         */
-
-
         for(int i = 0; i < this.size; i++)
         {
             this.roster[i].tuitionDue();
